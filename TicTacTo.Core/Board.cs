951567e15d5924f87425d37674726c3c4a385b9c
Dakota Jordan <heartoffiremedia@yahoo.com>
2023-11-29 11:07:05 -0600

    Added more unit tests for a more complete battery. Added more comments to core logic. Introduced backing field for Positions array in core logic.

--- TicTacTo.Core/Board.cs
+++ TicTacTo.Core/Board.cs
@@ -20,14 +20,15 @@
             [0, 4, 8],
             [2, 4, 6]
         ];
+        private readonly int[] positions = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+
         /// <summary>
         /// Current state of the game board.
         /// Initialized to -1 in all positions.
         /// Empty = -1, 'X' = 0, 'O' = 1
         /// Read Only
         /// </summary>
-        public int[] Positions { get; } = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-
+        public int[] Positions => positions;
         /// <summary>
         /// Updates the current positions of the board according to what the player chooses.
         /// </summary>
@@ -36,8 +37,8 @@
         /// <returns>False if the position has already been selected, otherwise true.</returns>
         public bool TakeTurn(int player, int position)
         {
-            if (Positions[position] != -1) return false;
-            Positions[position] = player;
+            if (positions[position] != -1) return false; //players can only play on an empty tile. Trying to play on an occupied tile will fail
+            positions[position] = player;
             return true;
         }
         /// <summary>
@@ -46,15 +47,15 @@
         /// <returns>-1 if no win was found, 0 if Player 1 'X' won, 1 if player 2 'O' won. 2 if stalemate</returns>
         public int CheckWin()
         {
-            foreach (var condition in winConditions)
+            foreach (var condition in winConditions) //Check for a win
             {
-                if (Positions[condition[0]] == Positions[condition[1]] && Positions[condition[0]] == Positions[condition[2]])
-                    return Positions[condition[0]];
+                if (positions[condition[0]] == positions[condition[1]] && positions[condition[0]] == positions[condition[2]]) // if a = b and a = c then b = c
+                    return positions[condition[0]]; //if a win is detected, then any/all of the tiles in the win condition contain the winning player
             }
 
             bool stalemate = true;
-            foreach (var position in Positions)
-                stalemate = position != -1 && stalemate;
+            foreach (var position in positions)          // Check for stalemate or incomplete game. This only runs if no outright win is detected. 
+                stalemate = position != -1 && stalemate; // Stalemate only changes state if it is still true and the board contains an unplayed tile
 
             return stalemate ? 2 : -1;
         }
--- TicTacTo.Testing/UnitTesting.cs
+++ TicTacTo.Testing/UnitTesting.cs
@@ -13,9 +13,30 @@ namespace TicTacTo.Testing
         }
 
         [Theory]
-        [InlineData(new int[9] { 0, 0, 0, 1, 1, 0, 1, 1, -1 }, 0)]
-        [InlineData(new int[9] { 1, 1, 1, 0, 0, 1, 0, 0, -1 }, 1)]
-        [InlineData(new int[9] { 1, 0, 1, 0, 0, 1, 0, 1, 0 }, 2)]
+        [InlineData(new int[9] { 0, 0, 0, 
+                                 1, 1, 0, 
+                                 1, 1, -1 }, 0)] //Player 1 win - Row
+        [InlineData(new int[9] { 1, 1, 1, 
+                                 0, 0, 1, 
+                                 0, 0, -1 }, 1)] //Player 2 win - Row
+        [InlineData(new int[9] { 1, 0, 0,
+                                 1, 0, 0,
+                                 1, 1, -1 }, 1)] //Player 2 win - Column
+        [InlineData(new int[9] { 1, 0, 1,
+                                 1, 0, 1,
+                                 0, 0, -1 }, 0)] //Player 1 win - Column
+        [InlineData(new int[9] { 0, 0, 1,
+                                 1, 1, 0,
+                                 1, 0, -1 }, 1)] //Player 2 win - Diagonal
+        [InlineData(new int[9] { 1, 1, 0,
+                                 1, 0, 1,
+                                 0, 0, -1 }, 0)] //Player 1 win - Diagonal
+        [InlineData(new int[9] { 1, 0, 1, 
+                                 0, 0, 1, 
+                                 0, 1, 0 }, 2)] //Player 1 win - Stalemate
+        [InlineData(new int[9] { 1, 0, 1,
+                                 0, 0, 1,
+                                 0, 1, -1 }, -1)] //Player 1 win - Incomplete Game
         public void Play(int[] positions, int expected)
         {
             for (int i = 0; i < positions.Length; i++)
