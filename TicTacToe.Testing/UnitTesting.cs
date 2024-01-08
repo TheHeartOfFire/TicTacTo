d24d55a568b84769a76f65758653299596318a07
Dakota Jordan-Laptop <heartoffiremedia@yahoo.com>
2024-01-07 22:45:10 -0600

    Board can now vary in size. Win conditions are now generated instead of being fixed.

--- TicTacToe.Testing/UnitTesting.cs
+++ TicTacToe.Testing/UnitTesting.cs
@@ -1,4 +1,5 @@
-﻿using TicTacToe.Core;
+﻿using System.Drawing;
+using TicTacToe.Core;
 using Xunit;
 using static TicTacToe.Core.Tile;
 using static TicTacToe.Core.WinResult;
@@ -7,12 +8,8 @@ namespace TicTacToe.Testing
 {
     public class UnitTesting
     {
-        private readonly Board _sut;
+        private Board _sut;
 
-        public UnitTesting()
-        {
-            _sut = new Board();
-        }
 
         [Theory]
         [InlineData(new TileOwner[9] { TileOwner.Player1, TileOwner.Player1, TileOwner.Player1,
@@ -39,8 +36,42 @@ namespace TicTacToe.Testing
         [InlineData(new TileOwner[9] { TileOwner.Player2, TileOwner.Player1, TileOwner.Player2,
                                  TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
                                  TileOwner.Player1, TileOwner.Player2, TileOwner.Unclaimed }, WinType.None)] //Player 1 win - Incomplete Game
-        public void Play(TileOwner[] owner, WinType expected)
+        [InlineData(new TileOwner[16] { TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, 
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Unclaimed }, WinType.Player1, 4)] //Player 1 win - Row
+        [InlineData(new TileOwner[16] { TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player2,
+                                        TileOwner.Player2, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player2, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Unclaimed }, WinType.Player2, 4)] //Player 2 win - Row
+        [InlineData(new TileOwner[16] { TileOwner.Player2, TileOwner.Player1, TileOwner.Player1, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player1, TileOwner.Player1, TileOwner.Unclaimed }, WinType.Player2, 4)] //Player 2 win - Column
+        [InlineData(new TileOwner[16] { TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Unclaimed }, WinType.Player1, 4)] //Player 1 win - Column
+        [InlineData(new TileOwner[16] { TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player1, TileOwner.Player1, TileOwner.Unclaimed }, WinType.Player2, 4)] //Player 2 win - Diagonal
+        [InlineData(new TileOwner[16] { TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player1, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Unclaimed }, WinType.Player1, 4)] //Player 1 win - Diagonal
+        [InlineData(new TileOwner[16] { TileOwner.Player1, TileOwner.Player1, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player1, TileOwner.Player2,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player1, TileOwner.Player1 }, WinType.Stalemate, 4)] //Player 1 win - Stalemate
+        [InlineData(new TileOwner[16] { TileOwner.Player1, TileOwner.Player1, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player2, TileOwner.Player2, TileOwner.Player2, TileOwner.Player1,
+                                        TileOwner.Player1, TileOwner.Player2, TileOwner.Player1, TileOwner.Unclaimed }, WinType.None, 4)] //Player 1 win - Incomplete Game
+
+        public void Play(TileOwner[] owner, WinType expected, int size = 3)
         {
+            _sut = new Board(size);
             for (int i = 0; i < owner.Length; i++)
             {
                 if (owner[i] is not TileOwner.Unclaimed)
