8cfa13c9fd6a0fa48b6b7c8ed778bc0a55a62c2e
Dakota Jordan <heartoffiremedia@yahoo.com>
2024-01-16 11:46:47 -0600

    Added simble bot that picks tiles randomly.

---
+++ TicTacToe.AI/BadBot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.AI.Interfaces;
+using TicTacToe.Core;
+using static TicTacToe.Core.Tile;
+
+namespace TicTacToe.AI;
+public class BadBot(TileOwner order) : ITicTacToeBot
+{
+    public Board TakeTurn(Board game, Tile tilePlayed) => PickTile(game);
+
+    private Board PickTile(Board game)
+    {
+        List<Tile> unclaimedTiles = new();
+
+        foreach(var tile in game.Positions)
+            if(tile.Owner is Tile.TileOwner.Unclaimed) unclaimedTiles.Add(tile);
+
+        var rand = new Random();
+
+        game.TakeTurn(order, rand.Next(0, unclaimedTiles.Count));
+        return game;
+
+    }
+}
---
+++ TicTacToe.AI/Interfaces/ITicTacToeBot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Core;
+
+namespace TicTacToe.AI.Interfaces;
+public interface ITicTacToeBot
+{
+    public Board TakeTurn(Board game, Tile tilePlayed);
+}
